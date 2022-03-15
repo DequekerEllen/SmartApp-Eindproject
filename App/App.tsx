c60216520bffc14ed8e5fee562186b3f9ea6c0e3
Ellen Dequeker <ellen.dequeker@student.howest.be>
2022-03-15 10:50:14 +0100

    tab bar

--- App/App.tsx
+++ App/App.tsx
@@ -1,20 +1,40 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Favorites from './screens/Favorites';
+import Home from './screens/Home';
+import Nearby from './screens/Nearby';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<NavigationContainer>
+			<StatusBar style="auto" />
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Explore') {
+							iconName = focused ? 'search' : 'search';
+						} else if (route.name === 'Favorites') {
+							iconName = focused ? 'heart' : 'heart';
+						} else if (route.name === 'Nearby') {
+							iconName = focused ? 'md-compass' : 'md-compass';
+						}
+
+						return <Ionicons name={iconName} size={size} color={color} />;
+					},
+					tabBarActiveTintColor: 'tomato',
+					tabBarInactiveTintColor: 'gray',
+				})}
+			>
+				<Tab.Screen name="Explore" component={Home} />
+				<Tab.Screen name="Favorites" component={Favorites} />
+				<Tab.Screen name="Nearby" component={Nearby} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ App/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    alpha: '#ba0f0f',
+    dark: '#2e0101',
+    light: '#f2eeeb',
+    grey: '#808080',
+  }
+  
